b78978a33c9717dcd609327863a4aa56e72ad262
Geka58 <he588@yandex.ru>
2025-02-17 22:30:46 +0300

    добавил word

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello word");
+        System.out.println("hello word");
         System.out.println("hello");
     }
 }
